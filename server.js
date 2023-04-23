2e9f298324856088255ea487311997fe0958708a
rodrigo72 <a100706@alunos.uminho.pt>
2023-04-23 14:21:49 +0100

    register limit implementado

--- server.js
+++ server.js
@@ -16,6 +16,16 @@ const errorHandler = require("./middleware/errorHandler");
 
 const verifyJWT = require("./middleware/verifyJWT");
 const cookieParser = require("cookie-parser");
+const rateLimit = require("express-rate-limit");
+
+const createAccountLimiter = rateLimit({
+	windowMs: 60 * 60 * 1000, // 1 hour
+	max: 3, 
+	message:
+		'Too many accounts created from this IP, please try again after an hour',
+	standardHeaders: true, // return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // disable the `X-RateLimit-*` headers
+})
 
 const PORT = process.env.PORT || 3500;
 
@@ -40,7 +50,7 @@ app.use("/signin", require("./routes/loginPage"));
 app.use("/signup", require("./routes/registerPage"));
 app.use("/add", require("./routes/add"));
 
-app.use("/register", require("./routes/register"));
+app.use("/register", createAccountLimiter, require("./routes/register"));
 app.use("/auth", require("./routes/auth"));
 app.use("/refresh", require("./routes/refresh"));
 app.use("/logout", require("./routes/logout"));
